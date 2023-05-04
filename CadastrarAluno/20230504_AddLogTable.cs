0bb69f5fb5bfd034815bca165206e0630b5744bb
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2023-05-04 13:30:23 -0300

    tabela criada

---
+++ CadastrarAluno/20230504_AddLogTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace CadastrarAluno
+{
+
+    [Migration(20230504114300)]
+    public class AddLogTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("tb_aluno")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Nome Aluno").AsString()
+                .WithColumn("CPF").AsString()
+                .WithColumn("Telefone").AsString()
+                .WithColumn("Data de Nascimento").AsDateTime();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("tb_aluno");
+        }
+    }
+}

e19933cac915d85da23fba92ddf0859ec8c21cdc
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2023-05-10 08:01:42 -0300

    metodo de conexao refatorado

--- CadastrarAluno/RepositorioBd.cs
+++ CadastrarAluno/RepositorioBd.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using System.Configuration;
 using System.Data;
+using System.Drawing;
 
 namespace CadastrarAluno
 {
@@ -8,8 +9,7 @@ namespace CadastrarAluno
 
     {
         private List<Aluno> lista = new List<Aluno>();
-        private static string conectionString = ConfigurationManager.ConnectionStrings["BancoDeAlunos"].ConnectionString;
-        SqlConnection con = new SqlConnection(conectionString);
+
 
         public List<Aluno> ObterTodos()
         {
@@ -18,157 +18,154 @@ namespace CadastrarAluno
                 lista = new List<Aluno>();
             }
 
-            try
+            using (var conexao = conectar())
             {
-                conectar();
-                string sql = "SELECT * FROM tb_aluno";
-                SqlCommand cmd = new SqlCommand(sql, con);
-                cmd.CommandType = CommandType.Text;
-                SqlDataReader dataReader = cmd.ExecuteReader();
-
-                while (dataReader.Read())
+                try
                 {
-                    lista.Add(new Aluno()
-                    {
-                        Id = Convert.ToInt32(dataReader["id"]),
-                        NomeAluno = dataReader["Nome"].ToString(),
-                        Cpf = dataReader["Cpf"].ToString(),
-                        Telefone = dataReader["Telefone"].ToString(),
-                        DataNascimento = Convert.ToDateTime(dataReader["Data_de_Nascimento"].ToString()),
-                    });
-                }
+                    string sql = "SELECT * FROM tb_aluno";
+                    SqlCommand cmd = new SqlCommand(sql, conexao);
+                    cmd.CommandType = CommandType.Text;
+                    SqlDataReader dataReader = cmd.ExecuteReader();
 
-                dataReader.Close();
+                    while (dataReader.Read())
+                    {
+                        lista.Add(new Aluno()
+                        {
+                            Id = Convert.ToInt32(dataReader["id"]),
+                            NomeAluno = dataReader["Nome"].ToString(),
+                            Cpf = dataReader["Cpf"].ToString(),
+                            Telefone = dataReader["Telefone"].ToString(),
+                            DataNascimento = Convert.ToDateTime(dataReader["Data_de_Nascimento"].ToString()),
+                        });
+                    }
+                    dataReader.Close();
+                    return lista;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
                 return lista;
             }
-            catch (Exception e)
-            {
-            }
-            finally { desconectar(); }
-            return lista;
         }
 
         public void Criar(Aluno novoAluno)
         {
-
-            try
+            using (var conexao = conectar())
             {
-                conectar();
-                string sql = "INSERT INTO tb_aluno (Nome, Cpf, Telefone, Data_de_Nascimento) VALUES (@Nome,@Cpf,@Telefone,@Data_de_Nascimento)";
-                SqlCommand cmd = new SqlCommand(sql, con);
+                try
+                {
+                    string sql = "INSERT INTO tb_aluno (Nome, Cpf, Telefone, Data_de_Nascimento) VALUES (@Nome,@Cpf,@Telefone,@Data_de_Nascimento)";
+                    SqlCommand cmd = new SqlCommand(sql, conexao);
 
-                cmd.Parameters.AddWithValue("@Nome", novoAluno.NomeAluno);
-                cmd.Parameters.AddWithValue("@Cpf", novoAluno.Cpf);
-                cmd.Parameters.AddWithValue("@Telefone", novoAluno.Telefone);
-                cmd.Parameters.AddWithValue("@Data_de_Nascimento", novoAluno.DataNascimento);
-                cmd.CommandType = CommandType.Text;
-                cmd.ExecuteNonQuery();
+                    cmd.Parameters.AddWithValue("@Nome", novoAluno.NomeAluno);
+                    cmd.Parameters.AddWithValue("@Cpf", novoAluno.Cpf);
+                    cmd.Parameters.AddWithValue("@Telefone", novoAluno.Telefone);
+                    cmd.Parameters.AddWithValue("@Data_de_Nascimento", novoAluno.DataNascimento);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
 
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
             }
-            catch (Exception e)
-            {
-                throw e;
-            }
-            finally
-            { desconectar(); }
+
         }
-
-
 
         public void Atualizar(int id, Aluno alunoASerAtualizado)
         {
-            try
+            using (var conexao = conectar())
             {
-                conectar();
-                string sql = "UPDATE tb_aluno SET Nome = @Nome, Cpf = @Cpf, Telefone = @Telefone, Data_de_Nascimento = @Data_de_Nascimento WHERE ID = @ID";
-                SqlCommand cmd = new SqlCommand(sql, con);
+                try
+                {
+                    conectar();
+                    string sql = "UPDATE tb_aluno SET Nome = @Nome, Cpf = @Cpf, Telefone = @Telefone, Data_de_Nascimento = @Data_de_Nascimento WHERE ID = @ID";
+                    SqlCommand cmd = new SqlCommand(sql, conexao);
 
-                cmd.Parameters.AddWithValue("@ID", alunoASerAtualizado.Id);
-                cmd.Parameters.AddWithValue("@Nome", alunoASerAtualizado.NomeAluno);
-                cmd.Parameters.AddWithValue("@Cpf", alunoASerAtualizado.Cpf);
-                cmd.Parameters.AddWithValue("@Telefone", alunoASerAtualizado.Telefone);
-                cmd.Parameters.AddWithValue("@Data_de_Nascimento", alunoASerAtualizado.DataNascimento);
-                cmd.CommandType = CommandType.Text;
-                cmd.ExecuteNonQuery();
+                    cmd.Parameters.AddWithValue("@ID", alunoASerAtualizado.Id);
+                    cmd.Parameters.AddWithValue("@Nome", alunoASerAtualizado.NomeAluno);
+                    cmd.Parameters.AddWithValue("@Cpf", alunoASerAtualizado.Cpf);
+                    cmd.Parameters.AddWithValue("@Telefone", alunoASerAtualizado.Telefone);
+                    cmd.Parameters.AddWithValue("@Data_de_Nascimento", alunoASerAtualizado.DataNascimento);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
 
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
             }
-            catch (Exception e)
-            {
-                throw e;
-            }
-            finally
-            { desconectar(); }
         }
 
 
         public Aluno ObterPorId(int id)
         {
             var buscarAluno = new Aluno();
-            try
+
+            using (var conexao = conectar())
             {
-                conectar();
-                string sql = $"SELECT FROM tb_aluno Where Id= {id}";
-                SqlCommand cmd = new SqlCommand(sql, con);
-                cmd.CommandType = CommandType.Text;
-                SqlDataReader dataReader = cmd.ExecuteReader();
-                while (dataReader.Read())
+                try
                 {
-                    buscarAluno = new Aluno()
+                    string sql = $"SELECT FROM tb_aluno Where Id= {id}";
+                    SqlCommand cmd = new SqlCommand(sql, conexao);
+                    cmd.CommandType = CommandType.Text;
+                    SqlDataReader dataReader = cmd.ExecuteReader();
+                    while (dataReader.Read())
                     {
-                        Id = Convert.ToInt32(dataReader["id"]),
-                        NomeAluno = dataReader["Nome"].ToString(),
-                        Cpf = dataReader["Cpf"].ToString(),
-                        Telefone = dataReader["Telefone"].ToString(),
-                        DataNascimento = Convert.ToDateTime(dataReader["Data_de_Nascimento"].ToString()),
-                    };
+                        buscarAluno = new Aluno()
+                        {
+                            Id = Convert.ToInt32(dataReader["id"]),
+                            NomeAluno = dataReader["Nome"].ToString(),
+                            Cpf = dataReader["Cpf"].ToString(),
+                            Telefone = dataReader["Telefone"].ToString(),
+                            DataNascimento = Convert.ToDateTime(dataReader["Data_de_Nascimento"].ToString()),
+                        };
+                    }
+                    dataReader.Close();
                 }
-                dataReader.Close();
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                return buscarAluno;
             }
-            catch(Exception e)
-            {
-                throw e;
-            }
-            finally
-            {
-                desconectar();
-            }
-            return buscarAluno;
 
         }
         public void Remover(int id)
         {
+            using (var conexao = conectar())
+            {
+                try
+                {
+                    string sql = $"DELETE FROM tb_aluno Where Id= {id}";
+                    SqlCommand cmd = new SqlCommand(sql, conexao);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+
+        private SqlConnection conectar()
+        {
+            string conectionString = ConfigurationManager.ConnectionStrings["BancoDeAlunos"].ConnectionString;
+            SqlConnection con = new SqlConnection(conectionString);
             try
             {
-                conectar();
-                string sql = $"DELETE FROM tb_aluno Where Id= {id}";
-                SqlCommand cmd = new SqlCommand(sql, con);
-                cmd.CommandType = CommandType.Text;
-                cmd.ExecuteNonQuery();
-
+                con.Open();
             }
             catch (Exception e)
             {
                 throw e;
             }
-            finally
-            { desconectar(); }
-        }
-
-        private SqlConnection conectar()
-        {
-            if (con.State == System.Data.ConnectionState.Closed)
-            {
-                con.Open();
-            }
             return con;
-        }
-
-        public void desconectar()
-        {
-            if (con.State == System.Data.ConnectionState.Open)
-            {
-                con.Close();
-            }
         }
     }
 }

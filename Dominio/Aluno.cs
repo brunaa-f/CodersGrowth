e8cd990c66e50e663564629188d01364e04cbc56
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2023-05-12 14:34:05 -0300

    mapeamento

--- Dominio/Aluno.cs
+++ Dominio/Aluno.cs
@@ -1,11 +1,19 @@
-﻿namespace Dominio
+﻿using LinqToDB.Mapping;
+
+namespace Dominio
 {
     public class Aluno
     {
+        [PrimaryKey, Identity]
         public int Id { get; set; }
+
+        [NotNull]
         public string Nome { get; set; }
+        [NotNull]
         public string Cpf { get; set; }
+        [NotNull]
         public string Telefone { get; set; }
+        [NotNull]
         public DateTime Nascimento { get; set; }
 
         public Aluno(int id, string nome, string cpf, string telefone, DateTime nascimento)
---
+++ Infra/RepositorioLinq2DB.cs
@@ -0,0 +1,42 @@
+﻿using Dominio;
+using LinqToDB.Data;
+using LinqToDB;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra
+{
+    internal class RepositorioLinq2DB : IRepositorio
+    {
+
+        public void Atualizar(int id, Aluno alunoASerAtualizado)
+        {
+        }
+
+        public void Criar(Aluno novoAluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

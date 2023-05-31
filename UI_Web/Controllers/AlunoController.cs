0678639965740eb27c81366c1e6495d957b45498
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2023-05-31 14:43:21 -0300

    implementados todos os endpoints

--- UI_Web/Controllers/AlunoController.cs
+++ UI_Web/Controllers/AlunoController.cs
@@ -1,3 +1,4 @@
+using CadastrarAluno.Servicos;
 using Dominio;
 using Infra;
 using Microsoft.AspNetCore.Mvc;
@@ -7,7 +8,7 @@ namespace UI_Web.Controllers
     [ApiController]
     [Route("api/[controller]")]
 
-    public class AlunoController : ControllerBase    
+    public class AlunoController : ControllerBase
     {
         private IRepositorio _aluno;
 
@@ -16,19 +17,83 @@ namespace UI_Web.Controllers
             _aluno = aluno;
         }
 
-        [HttpPost]
+        [HttpGet]
         public IActionResult ObterTodos()
         {
-           try
+            try
             {
-                List<Aluno> aluno = _aluno.ObterTodos();
+                List<Aluno> alunos = _aluno.ObterTodos();
+                return Ok(alunos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult Criar(Aluno novoAluno)
+        {
+            try
+            {
+                ValidarForm.Valida(novoAluno);
+                _aluno.Criar(novoAluno);    
+                return Created($"{novoAluno.Id}", novoAluno);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId([FromRoute] int Id)
+        {
+            try
+            {
+             Aluno aluno = _aluno.ObterPorId(Id);
+             return Ok(aluno);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remover([FromRoute]int id)
+        {
+            try
+            {
+                var aluno = _aluno.ObterPorId(id);
+                _aluno.Remover(id);
                 return Ok(aluno);
             }
             catch (Exception ex)
             {
-              return BadRequest(ex.Message);
+                return BadRequest(ex.Message);
             }
         }
 
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar([FromRoute] int id, [FromBody] Aluno aluno)
+        {
+            try
+            {
+                ValidarForm.Valida(aluno);
+                _aluno.Atualizar(id, aluno);
+                return Ok(aluno.Id);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }

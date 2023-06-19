f939283b00a045fbe258ed0f9397a6e51d988b2a
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2023-06-19 13:58:24 -0300

    fetch

--- UI_Web/wwwroot/controller/TelaInicial.controller.js
+++ UI_Web/wwwroot/controller/TelaInicial.controller.js
@@ -1,11 +1,25 @@
-sap.ui.define(["sap/ui/core/mvc/Controller",
-], function (Controller) {
-    "use strict";
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel",
+        "sap/ui/model/Filter",
+        "sap/ui/model/FilterOperator",
+        "sap/ui/core/routing/Router",
+    ],
+    function (Controller, JSONModel, Filter, FilterOperator) {
+        "use strict";
+        return Controller.extend("ControleDeAlunos.Controller.TelaInicial", {
+            onInit: function () {
+                let oView = this.getView();
 
-    return Controller.extend("ControleDeAlunos.controller.TelaInicial"), {
-
-        onInit: function () {
-            this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-        },
-    }
-});
+                fetch("/api/Aluno")
+                    .then((response) => response.json())
+                    .then((data) => {
+                        oView.setModel(new JSONModel(data), "alunos");
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            },
+        })
+    })

4e0e7437d64e978becf24552a2d97600d992e85b
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2023-06-19 12:14:57 -0300

    atualização

--- UI_Web/wwwroot/controller/App.controller.js
+++ UI_Web/wwwroot/controller/App.controller.js
@@ -1,10 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-], function (
-    Controller
-) {
+
+], function (Controller) {
     "use strict";
     return Controller.extend("ControleDeAlunos.controller.App", {
+
     });
 });
 

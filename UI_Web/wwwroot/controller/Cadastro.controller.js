af1ab565637a3ffba5ba616264e109906da94869
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2023-06-27 11:46:14 -0300

    troca do tema

---
+++ UI_Web/wwwroot/controller/Cadastro.controller.js
@@ -0,0 +1,29 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/routing/History"
+], function (Controller, JSONModel, History) {
+    "use strict";
+    return Controller.extend("ControleDeAlunos.controller.Cadastro", {
+
+
+        onInit: function () {
+        },
+
+        aoClicarVoltar: function () {
+            const rotaTelaInicial = "TelaInicial"
+
+            var caminhoAnterior = History.getInstance().getPreviousHash();
+            if (caminhoAnterior !== undefined) {
+                window.history.go(-1);
+            } else {
+                var rota = this.getOwnerComponent().getRouter();
+                rota.navTo(rotaTelaInicial);
+            }
+        },
+
+        aoClicarSalvar: function () {
+        }
+
+    });
+});
